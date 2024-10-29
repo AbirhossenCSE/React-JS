772f0c140089ff0cea735ae726d397352700bb75
Abir Hossen <abir.cse4@gmail.com>
2024-10-29 22:13:06 +0600

    last

--- price-options-1/src/components/PriceOption/PriceOptioon.jsx
+++ price-options-1/src/components/PriceOption/PriceOptioon.jsx
@@ -1,8 +1,13 @@
 import PropTypes from "prop-types";
+
 const PriceOptioon = ({option}) => {
+    const {name, price, features} = option;
     return (
         <div>
-            
+            <h3>
+                <span className="text-6xl">{price}</span>
+                <span className="text-3xl">/Month</span>
+            </h3>
         </div>
     );
 };
