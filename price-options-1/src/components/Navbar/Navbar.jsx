7bec797aec3996d8a83f8130f687ddac467d8438
Abir Hossen <abir.cse4@gmail.com>
2024-10-29 21:53:40 +0600

    navbar

--- price-options-1/src/components/Navbar/Navbar.jsx
+++ price-options-1/src/components/Navbar/Navbar.jsx
@@ -13,19 +13,19 @@ const Navbar = () => {
 
 
     return (
-        <div>
+        <nav className="text-black p-6 bg-yellow-300">
             <div className="md:hidden" onClick={() => setOpen(!open)}>
                 {
-                    open === true ? <IoMdMenu className="text-2xl"></IoMdMenu> : <IoMdClose></IoMdClose>
+                    open === true ? <IoMdClose></IoMdClose> : <IoMdMenu className="text-2xl"></IoMdMenu>
                 }
                 
             </div>
-            <ul className="flex">
+            <ul className={`md:flex duration-1000 absolute md:static  p-4 ${open ? 'top-16' : '-top-60' }`}>
                 {
                     routes.map(route => <Link key={route.id} route={route}></Link> )
                 }
             </ul>
-        </div>
+        </nav>
     );
 };
 
