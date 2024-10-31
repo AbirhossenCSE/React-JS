832d889c40565a25bee63636f68152c0822365f3
Abir Hossen <abir.cse4@gmail.com>
2024-10-31 13:12:00 +0600

    path

--- introduction-react-router/src/main.jsx
+++ introduction-react-router/src/main.jsx
@@ -1,10 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from './App.jsx'
+import "./index.css"
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <div>Hello From react router</div>
+  },
+  {
+    path: '/about',
+    element: <div>I am in the about page</div>
+  }
+])
+
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router}></RouterProvider>
+  </React.StrictMode>
+);
