3f5fe5c512d863abb1b652999937f31594150a70
Abir Hossen <abir.cse4@gmail.com>
2024-10-31 13:40:00 +0600

    nasted route

---
+++ introduction-react-router/src/components/About/About.jsx
@@ -0,0 +1,11 @@
+
+const About = () => {
+    return (
+        <div>
+          <h2>About Page</h2> 
+          <p>What you want to know</p> 
+        </div>
+    );
+};
+
+export default About;
---
+++ introduction-react-router/src/components/Contact/Contact.jsx
@@ -0,0 +1,11 @@
+
+const Contact = () => {
+    return (
+        <div>
+            <h2>You want to contact with me</h2>
+            <p>Number: 0123456789</p>
+        </div>
+    );
+};
+
+export default Contact;
