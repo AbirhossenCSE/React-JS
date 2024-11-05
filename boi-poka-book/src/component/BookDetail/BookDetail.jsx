59edc80efa1c43db0e4192f637458e1cb47eddd4
Abir Hossen <abir.cse4@gmail.com>
2024-11-06 00:43:19 +0600

    mark as read

--- boi-poka-book/src/component/BookDetail/BookDetail.jsx
+++ boi-poka-book/src/component/BookDetail/BookDetail.jsx
@@ -1,4 +1,5 @@
 import { useLoaderData, useParams } from "react-router-dom";
+import { addToStoreReadList } from "../../utility/addToDb";
 
 const BookDetail = () => {
     
@@ -9,12 +10,17 @@ const BookDetail = () => {
     const book = data.find(book => book.bookId === id)
 
     const {bookId: currentBookId, image} = book;
+
+    const handleMarkAsRead = (id) =>{
+        addToStoreReadList(id);
+    }
+
     return (
         <div className="m-4 p-4">
             <h2>Book Details: {bookId} </h2>
             <img className="w-36" src={image} alt="" />
-            <button className="btn btn-outline mr-4 btn-accent">Read</button>
-            <button className="btn btn-accent">Wish List</button>
+            <button onClick={() => handleMarkAsRead(bookId)} className="btn btn-outline mr-4 btn-accent">Mark  As Read</button>
+            <button className="btn btn-accent">Add to Wish List</button>
         </div>
     );
 };
